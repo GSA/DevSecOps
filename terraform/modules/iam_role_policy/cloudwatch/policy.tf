d47ebeaeb66b985332eea154069d456354224367
Vermyndax <jmiller@red-abstract.com>
2018-01-15 17:18:37 +0000

    Removing terraform directory

--- terraform/modules/iam_role_policy/cloudwatch/policy.tf
+++
@@ -1,8 +0,0 @@
-data "template_file" "policy" {
-  template = "${file("${path.module}/policy.json")}"
-}
-
-resource "aws_iam_policy" "iam_policy" {
-  name = "${var.policy_name}"
-  policy = "${data.template_file.policy.rendered}"
-}
--- terraform/modules/vpc_flow_log/variables.tf
+++
@@ -1,4 +0,0 @@
-variable vpc_id {
-}
-variable vpc_name {
-}
